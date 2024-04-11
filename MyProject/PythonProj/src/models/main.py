add788588a44558f361f5eef4529f75fdfc39490
meny2 <164458102+meny2@users.noreply.github.com>
2024-04-11 22:11:25 +0700

    Delete MyProject directory

--- MyProject/PythonProj/src/models/main.py
+++
@@ -1,9 +0,0 @@
-from database import connect_to_database
-from check_login import check_user_login
-
-# Establish database connection
-connection = connect_to_database()
-
-# Call function to check user login
-if connection:
-    check_user_login(connection, "admin", "adminpwd")
